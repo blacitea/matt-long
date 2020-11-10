43caf943bf38194cffabbf1457e2d5ea60f3491b
Angel <blacitea@gmail.com>
2020-11-10 12:34:27 -0800

    add button to link to CV

--- src/Bio.jsx
+++ src/Bio.jsx
@@ -14,12 +14,14 @@ const Bio = () => {
 				<p>
 					{' '}
 					Matt was first brought into an action film by his teammate Kyle Wong
-					who was in charge of the Choreography for the film The Grid (2016).
-					Here Matt put his great understanding of body and motion carefully
-					crafted from his years of Wushu competitions and coaching to good use.
-					He was hooked and has since sought out every opportunity to continue
-					his exploration of physicality in The Line (2018), and The Crow:
-					Rebirth (2017).
+					who was in charge of the Choreography for the film{' '}
+					<a href="https://youtu.be/m034ak3e0ic">The Grid (2016)</a>. Here Matt
+					put his great understanding of body and motion carefully crafted from
+					his years of Wushu competitions and coaching to good use. He was
+					hooked and has since sought out every opportunity to continue his
+					exploration of physicality in{' '}
+					<a href="https://vimeo.com/220861882">The Line (2018)</a>, and{' '}
+					<a href="https://youtu.be/71nUh7loQsk">The Crow: Rebirth (2017)</a>.
 				</p>{' '}
 				<p>
 					{' '}
@@ -28,6 +30,13 @@ const Bio = () => {
 					explore the different faces of emotionally varied characters in
 					different social settings.
 				</p>
+				<a
+					href="https://docs.google.com/document/d/1uvSXloq4eICTv5GkAP0SivYiGD4aC9ClJyuUxDms1kU/edit"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					<button className="button-CV">Matt Long's CV</button>
+				</a>
 			</article>
 			<section className="bio-image">
 				<aside className="m-2 p-1 flex-1 max-w-sm mx=auto shadow">
